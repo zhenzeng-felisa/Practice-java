22877e23b876087b4498370969d2f191699debd6
lightning_felisa <62801580+zhenzeng-felisa@users.noreply.github.com>
2020-04-04 10:30:35 +0545

    Coursera:<Java Programming>

---
+++ W2 Storing All Genes.java
@@ -0,0 +1,24 @@
+public String mystery(String dna) {
+  int pos = dna.indexOf("T");
+  int count = 0;
+  int startPos = 0;
+  String newDna = "";
+  if (pos == -1) {
+    return dna;
+  }
+  while (count < 3) {
+    count += 1;
+    newDna = newDna + dna.substring(startPos,pos);
+    startPos = pos+1;
+    pos = dna.indexOf("T", startPos);
+    if (pos == -1) {
+      break;
+    }
+  }
+  newDna = newDna + dna.substring(startPos);
+  return newDna;
+}
+
+ATBTABCS
+AB
+POS=0
